53b33e4d3138fdd3e4a62425e7b9ac608cee89d6
ivan <vannok@live.com>
2024-02-14 00:10:32 +0300

    initial setup

---
+++ handler_readiness.go
@@ -0,0 +1,8 @@
+package main
+
+import "net/http"
+
+func handlerReadiness(w http.ResponseWriter, _ *http.Request) {
+	respondWithJSON(w, http.StatusOK, struct {
+	}{})
+}
--- main.go
+++ main.go
@@ -1,5 +1,51 @@
 package main
 
-func main() {
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+)
 
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Can't load env")
+	}
+
+	portString := os.Getenv("PORT")
+
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerError)
+
+	router.Mount("/v1", v1Router)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Panic("Can't start the server")
+	}
 }
